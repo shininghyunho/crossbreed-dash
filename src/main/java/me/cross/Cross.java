5cd6aacd4cdca23ea84559da474894bb4271d143
shininghyunho <gusgh3315@uos.ac.kr>
2024-02-09 20:22:00 +0900

    철골렘 에그 예제

--- src/main/java/me/cross/Cross.java
+++ src/main/java/me/cross/Cross.java
@@ -3,11 +3,15 @@ package me.cross;
 import me.cross.entity.CubeEntity;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricDefaultAttributeRegistry;
 import net.fabricmc.fabric.api.object.builder.v1.entity.FabricEntityTypeBuilder;
 import net.minecraft.entity.EntityDimensions;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.SpawnGroup;
+import net.minecraft.item.*;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -25,10 +29,18 @@ public class Cross implements ModInitializer {
 					.dimensions(EntityDimensions.fixed(0.75F, 0.75F))
 					.build()
 	);
+
+	public static final Item IRON_GOLDEN_SPAWN_EGG = new SpawnEggItem(EntityType.IRON_GOLEM, 0xc4c4c4, 0xadadad, new FabricItemSettings());
+
 	@Override
 	public void onInitialize() {
 		LOGGER.info("Hello Fabric world!");
-
+		// add the spawn egg to MISC item group
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.SPAWN_EGGS).register(content -> content.add(IRON_GOLDEN_SPAWN_EGG));
+		// Register the spawn egg
+		Registry.register(Registries.ITEM,new Identifier("cross","iron_golem_spawn_egg"),IRON_GOLDEN_SPAWN_EGG);
+		// Register the entity
 		FabricDefaultAttributeRegistry.register(CUBE, CubeEntity.createMobAttributes());
+
 	}
 }
