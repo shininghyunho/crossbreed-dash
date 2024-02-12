63eb3beeb178d0da1811c2ebfea1d322d59e32df
shininghyunho <gusgh3315@uos.ac.kr>
2024-02-12 12:06:44 +0900

    블록파괴 예제 이벤트 추가

--- src/main/java/me/cross/Cross.java
+++ src/main/java/me/cross/Cross.java
@@ -2,6 +2,11 @@ package me.cross;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageType;
+import net.minecraft.util.ActionResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,7 +21,18 @@ public class Cross implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+		registerEvents();
 		LOGGER.info("Hello Fabric world!");
+	}
+
+	private void registerEvents() {
+		// Register the ServerMixin class to the server
+		AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
+			BlockState blockState = world.getBlockState(pos);
+			if(blockState.isToolRequired() && !player.isSpectator() && player.getMainHandStack().isEmpty()) {
+				LOGGER.info("블록 파괴 시도 이벤트 발생");
+			}
+			return ActionResult.PASS;
+		});
 	}
 }
