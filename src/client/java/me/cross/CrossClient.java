99fd90f14a407cbe065ab3eeac258ceb7ef307ea
shininghyunho <gusgh3315@uos.ac.kr>
2024-02-12 11:09:42 +0900

    클라이언트 인스턴스만 접근 가능함.

--- src/client/java/me/cross/CrossClient.java
+++ src/client/java/me/cross/CrossClient.java
@@ -1,8 +1,11 @@
 package me.cross;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.MinecraftClient;
 
 public class CrossClient implements ClientModInitializer {
+	// 서버 인스턴스는 접근이 불가능하지만 클라이언트 인스턴스는 접근이 가능.
+	MinecraftClient client = MinecraftClient.getInstance();
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
