adbbd51b5af5c791f7e80f309aad194f7726c3b5
shininghyunho <gusgh3315@uos.ac.kr>
2024-02-13 17:01:39 +0900

    moved racing enum

--- src/main/java/me/cross/handler/RacingHandler.java
+++ src/main/java/me/cross/handler/RacingHandler.java
@@ -1,6 +1,16 @@
 package me.cross.handler;
 
 public class RacingHandler {
+    public enum RACING_MOD {
+        // 경기가 시작되지 않음
+        NOT_STARTED,
+        // 출발선에서 경주 준비
+        READY,
+        // 경주 중
+        RUNNING,
+        // 경주 종료
+        FINISHED
+    }
     static RACING_MOD racingMod = RACING_MOD.NOT_STARTED;
 
     public static void ready() {
