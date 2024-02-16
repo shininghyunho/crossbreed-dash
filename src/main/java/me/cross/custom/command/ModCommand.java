dc1c986ff0a04cc5d0f415d46fb0feef9686a2a2
shininghyunho <gusgh3315@uos.ac.kr>
2024-02-16 16:49:28 +0900

    add result command

--- src/main/java/me/cross/custom/command/ModCommand.java
+++ src/main/java/me/cross/custom/command/ModCommand.java
@@ -3,6 +3,7 @@ package me.cross.custom.command;
 import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import me.cross.Cross;
+import me.cross.handler.RunningHandler;
 import net.minecraft.server.command.ServerCommandSource;
 
 import static net.minecraft.server.command.CommandManager.literal;
@@ -18,6 +19,12 @@ public final class ModCommand {
         // execute stopMod
         dispatcher.register(literal("stop").executes(context -> {
             Cross.stopMod();
+            return Command.SINGLE_SUCCESS;
+        }));
+
+        // get result
+        dispatcher.register(literal("result").executes(context -> {
+            Cross.broadcast(RunningHandler.getTotalResult());
             return Command.SINGLE_SUCCESS;
         }));
     }
