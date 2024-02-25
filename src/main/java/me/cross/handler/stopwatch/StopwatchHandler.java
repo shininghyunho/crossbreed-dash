78e11a51b2492fe1f2accb84cee7e33678e32e72
shininghyunho <gusgh3315@uos.ac.kr>
2024-02-25 21:02:48 +0900

    Edit 테스트를 위해 대기 시간 조정

--- src/main/java/me/cross/handler/stopwatch/StopwatchHandler.java
+++ src/main/java/me/cross/handler/stopwatch/StopwatchHandler.java
@@ -3,7 +3,7 @@ package me.cross.handler.stopwatch;
 import me.cross.handler.race.RacingMode;
 
 public class StopwatchHandler {
-    private static final long RACING_INTERVAL_SEC = 5, RUNNING_READY_SEC = 3, COUNTDOWN_SEC = 5, FINISHED_SEC = 10;
+    private static final long RACING_INTERVAL_SEC = 60, RUNNING_READY_SEC = 10, COUNTDOWN_SEC = 5, FINISHED_SEC = 10;
     public static final Stopwatch forNotStarted = new Stopwatch(RACING_INTERVAL_SEC, RacingMode.NOT_STARTED);
     public static final Stopwatch forRunningReady = new Stopwatch(RUNNING_READY_SEC, RacingMode.READY_FOR_RUNNING);
     public static final Stopwatch forCountdown = new Stopwatch(COUNTDOWN_SEC, RacingMode.COUNTDOWN);
